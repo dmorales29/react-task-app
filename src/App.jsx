69c332c5b8eea016ee1305ece3cea2b628d61962
David <davidmm_90@hotmail.com>
2022-12-26 22:03:04 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import TaskList from "./TaskList";
+
+export default function HolaMundo() {
+  return <TaskList />;
+}
---
+++ src/task.js
@@ -0,0 +1,27 @@
+const tasks = [
+  {
+    id: 0,
+    title: "Tarea 1",
+    description: "Mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "Tarea 2",
+    description: "Mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Tarea 3",
+    description: "Mi tercer tarea",
+  },
+  {
+    id: 3,
+    title: "Tarea 4",
+    description: "Mi cuarta tarea",
+  },
+  {
+    id: 4,
+    title: "Tarea 5",
+    description: "Mi quinta tarea",
+  },
+];
